3a8e2c1fb30e93a65d89a87de528702770572025
Alem Haile <wellma0043@gmail.com>
2022-03-17 00:37:53 +0300

    did a task

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
 /**
- * @brief description
+ * main - Entry point
  * 
- * @return int 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i = 0;
 	int sum = 0;
 
-	for (; i<1024; i++)
+	for (; i < 1024; i++)
 		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
 
