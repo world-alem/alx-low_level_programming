bc4bd9b59738f5be2bb345f3f93a56d82aaf3b2b
Alem Haile <wellma0043@gmail.com>
2022-03-17 00:32:39 +0300

    did a task

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 
+/*
+ * This is a description
+ */
 int main(void)
 {
-	int i=0;
+	int i = 0;
+	int sum = 0;
 
 	for (; i<1024; i++)
 		if (i % 3 == 0 || i % 5 == 0)
-			printf("%d\n", i);
+			sum += i;
 
 
+	printf("%d\n", sum);
 	return (0);
 }
