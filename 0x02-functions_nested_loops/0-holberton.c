755ce17291e662f9508ed5d13189d231bcf1fa71
Alem Haile <wellma0043@gmail.com>
2022-03-17 00:08:27 +0300

    did a task

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char h[] = "Holberton";
+	int m;
+
+	m = 0;
+
+	while (m <= 8)
+	{
+		_putchar(h[m]);
+		m++;
+	}
+	_putchar('\n');
+	return (0);
+}
