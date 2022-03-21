46f9fc2e38b7ef02632c99af179d378bff289df3
Alem Haile <wellma0043@gmail.com>
2022-03-21 22:44:26 +0300

    did a task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+ * swap_int - swaps the values of two integers
+ * @a: number 1
+ * @b: number 2
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
