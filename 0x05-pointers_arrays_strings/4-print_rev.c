0cfd4f8005a53ce2a2b6a4972e15d5ad26fbf8f3
Alem Haile <wellma0043@gmail.com>
2022-03-21 23:21:37 +0300

    did a task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * print_rev - print reverse of string
+ * @s: the string
+ */
+void print_rev(char *s)
+{
+	int i = strlen(*s);
+
+	while (i >= 0)
+	{
+		printf("%c", s[i]);
+		i--;
+	}
+}
