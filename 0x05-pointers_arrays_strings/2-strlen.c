8ac412df54c63c2de397447405a7031efc657115
Alem Haile <wellma0043@gmail.com>
2022-03-21 23:06:33 +0300

    did a task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+/*
+ * _strlen - returns length of string
+ * @s: the string
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for(i = 0; s[i] != '\0'; ++i);
+
+	return (i);
+}
