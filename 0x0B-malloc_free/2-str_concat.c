e772175a421c4812916c0e89133d9facb74d9d5e
Alem Haile <wellma0043@gmail.com>
2022-04-27 22:15:49 +0300

    did a task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -58,21 +58,21 @@ char *str_concat(char *s1, char *s2)
 	int length;
 	char *space;
 
-	if (s1 == '\0')
+	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-	if (s2 == '\0')
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}
 
 	length = _strlen(s1) + _strlen(s2);
-	if (s1 != '\0' && s2 != '\0')
+	if (s1 != NULL && s2 != NULL)
 	{
 		space = malloc(sizeof(char) * length + 1);
 
-		if (space == '\0')
+		if (space == NULL)
 		{
 			return ('\0');
 		}
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,7 +11,7 @@ void free_grid(int **grid, int height)
 {
 	int a;
 
-	if (grid == '\0' || height <= 0)
+	if (grid == NULL || height <= 0)
 	{
 		return;
 	}
