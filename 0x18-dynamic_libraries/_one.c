244a5ead2d43e48e022299a0e05e6c197808f7a2
Alem Haile <welma0043@gmail.com>
2022-06-18 00:44:06 +0300

    in one go

---
+++ 0x18-dynamic_libraries/_one.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _putchar - putchar
+ * @c: char
+ * Return: int
+ */
+int _putchar(char c)
+{
+}
+
+/**
+ * _islower - islower
+ * @c: int
+ * Return: int
+ */
+int _islower(int c)
+{
+}
+
+/**
+ * _isalpha - isalpha
+ * @c: int
+ * Return: int
+ */
+int _isalpha(int c)
+{
+}
+
+/**
+ * _abs - abs
+ * @n: int
+ * Return: int
+ */
+int _abs(int n)
+{
+}
+
+/**
+ * _isupper - isupper
+ * @c: int
+ * Return: int
+ */
+int _isupper(int c)
+{
+}
---
+++ 0x18-dynamic_libraries/_two.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _isdigit - isdigit
+ * @c: int
+ * Return: int
+ */
+int _isdigit(int c) {}
+
+/**
+ * _strlen - strlen
+ * @s: char*
+ * Return: int
+ */
+int _strlen(char *s) {}
+
+/**
+ * _puts - puts
+ * @s: char*
+ */
+void _puts(char *s) {}
+
+/**
+ * _strcpy - strcpy
+ * @dest: char*
+ * @src: char
+ * Return: char*
+ */
+char *_strcpy(char *dest, char *src);
+/**
+ * _atoi - atoi
+ * @s: char*
+ * Return: int
+ */
+int _atoi(char *s) {}
