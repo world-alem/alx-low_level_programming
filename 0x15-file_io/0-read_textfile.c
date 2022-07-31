1f842dd6a598131dcfc53ce1a5932f262b7af00a
Alem Haile <welma0043@gmail.com>
2022-07-31 22:35:25 +0300

    fix stuff

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * read_textfile - a function that reads a
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * create_file - a function that creates a file
