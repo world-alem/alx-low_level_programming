6005cc33d745415fa1406394813c181ca9ee2629
Alem Haile <welma0043@gmail.com>
2022-10-08 00:48:22 +0300

    did task

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,9 +18,10 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 
 	index = -1;
-	
+
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			index = i;
